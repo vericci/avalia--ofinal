de8dd548554e31a7990a44c9266718221679bc49
vericci <afabeth2222@gmail.com>
2024-11-22 15:31:24 -0300

    a

--- script.js
+++ script.js
@@ -35,6 +35,17 @@ const typed = new Typed('#typed-text', {
 });
 
 
+//Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
+hamburgerButton.addEventListener("click", function() {
+    mobileMenu.classList.add("flex");
+    //Criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (Div) aparecer, ou seja, ficar visível.
+});
+
+
+closeButton.addEventListener("click",function(){
+    mobileMenu.classList.remove("flex");
+});
+
 
        
 
